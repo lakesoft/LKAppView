f1c52226bb0eca3dff64e3cdba8dc74e9912025f
Hiroshi Hashiguchi <hashiguchi@lakesoft.jp>
2014-02-13 21:52:59 +0900

    Initial Commit

---
+++ LKAppView/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  LKAppView
+//
+//  Created by Hiroshi Hashiguchi on 2014/02/13.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKAppView/LKViewController.h
@@ -0,0 +1,13 @@
+//
+//  LKViewController.h
+//  LKAppView
+//
+//  Created by Hiroshi Hashiguchi on 2014/02/13.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKViewController : UIViewController
+
+@end
